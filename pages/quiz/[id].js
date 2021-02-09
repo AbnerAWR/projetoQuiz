7a37d1065d95d5ad2c70248fd9fd2c88a6b31850
AbnerAWR <AbnerW_@hotmail.com>
2021-02-09 20:10:55 -0300

    Quiz update

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,9 +1,36 @@
-export default function QuizDaGalera() {
+import { ThemeProvide } from 'styled-components'
+import QuizScreen from '../../src/screens/Quiz'
+
+export default function QuizDaGalera({ dbExterno }) {
     return (
-        <div>
-            <h1>
-                teste
-            </h1>
-        </div>
+        <ThemeProvide theme={dbExterno.theme}>
+            <QuizScreen 
+                externalQuestions={dbExterno.question}
+                externalBg={dbExterno.bg}
+            />
+        </ThemeProvide>
     )
+}
+
+export async function getServicesSideProps(context) {
+    const [projectName, githubUser] = context.query.id.split('__')
+
+    try {
+        const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
+            .then((respondeServer) => {
+                if(respostaDoServer.ok){
+                    return respondeServer.json()
+                }
+                throw new Error ('Falha em pegar os dados')
+            })
+            .then((responstaConvertidaEmObjeto) => responstaConvertidaEmObjeto)
+
+        return {
+            props:
+                dbExterno,
+        }
+
+    } catch(err) {
+        throw new Error(err);
+    }
 }
---
+++ pages/quiz/index.js
@@ -0,0 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import QuizScreen from '../../src/screens/Quiz';
+import db from '../../db.json';
+
+export default function QuizDaGaleraPage() {
+    return (
+        <ThemeProvider theme={db.theme}>
+            <QuizScreen 
+                externalQuestion={db.questions}
+                externalBg={db.bg} />
+        </ThemeProvider>
+    )
+}
