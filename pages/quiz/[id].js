711f7c8b8f4016b440a19f8d88969e3654e04bdb
AbnerAWR <AbnerW_@hotmail.com>
2021-02-10 18:01:00 -0300

    quiz page update

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,36 +1,36 @@
-import { ThemeProvide } from 'styled-components'
-import QuizScreen from '../../src/screens/Quiz'
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import QuizScreen from '../../src/screens/Quiz';
 
-export default function QuizDaGalera({ dbExterno }) {
-    return (
-        <ThemeProvide theme={dbExterno.theme}>
-            <QuizScreen 
-                externalQuestions={dbExterno.question}
-                externalBg={dbExterno.bg}
-            />
-        </ThemeProvide>
-    )
+export default function QuizDaGaleraPage({ dbExterno }) {
+  return (
+    <ThemeProvider theme={dbExterno.theme}>
+      <QuizScreen
+        externalQuestions={dbExterno.questions}
+        externalBg={dbExterno.bg}
+      />
+    </ThemeProvider>
+  );
 }
 
-export async function getServicesSideProps(context) {
-    const [projectName, githubUser] = context.query.id.split('__')
+export async function getServerSideProps(context) {
+  const [projectName, githubUser] = context.query.id.split('___');
 
-    try {
-        const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
-            .then((respondeServer) => {
-                if(respostaDoServer.ok){
-                    return respondeServer.json()
-                }
-                throw new Error ('Falha em pegar os dados')
-            })
-            .then((responstaConvertidaEmObjeto) => responstaConvertidaEmObjeto)
-
-        return {
-            props:
-                dbExterno,
+  try {
+    const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
+      .then((respostaDoServer) => {
+        if (respostaDoServer.ok) {
+          return respostaDoServer.json();
         }
-
-    } catch(err) {
-        throw new Error(err);
-    }
+        throw new Error('Falha em pegar os dados');
+      })
+      .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto)
+    return {
+      props: {
+        dbExterno,
+      },
+    };
+  } catch(err) {
+    throw new Error(err);
+  }
 }
