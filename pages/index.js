3729c34cd8fc91be18a675ef424d371e654d8900
AbnerAWR <AbnerW_@hotmail.com>
2021-02-10 18:21:28 -0300

    index update

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
               />
 
               <Button type="submit" disabled={name.length === 0}>
-                Jogar {name}
+                Jogar
               </Button>
 
             </form>
